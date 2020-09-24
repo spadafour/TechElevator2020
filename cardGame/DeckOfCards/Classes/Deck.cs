0c4e7d93dd055a9de9386fe8e441ebb980cc9510
Rob Spadafore <spadafour@gmail.com>
2020-09-24 13:29:19 -0400

    pushing card program workalong code

---
+++ cardGame/DeckOfCards/Classes/Deck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace DeckOfCards.Classes
+{
+    public class Deck
+    {
+        public int numberOfCardsInDeck { get; private set; }
+        
+        private List<Card> AllCards { get; set; } = new List<Card>();
+
+        public Deck()
+        {
+            foreach (string suit in Card.suitNames)
+            {
+                for (int i=1; i<14; i++)
+                {
+                    Card card = new Card(i, suit);
+                    AllCards.Add(card);
+                    numberOfCardsInDeck++;
+                }
+            }
+        }
+
+        public Card Deal1()
+        {
+            Card cardToBeDealt = AllCards[0];
+            AllCards.RemoveAt(0);
+            return AllCards[0];
+            
+        }
+
+
+        public void Shuffle()
+        {
+            Random r = new Random();
+            for (int i=0; i <100; i++)
+            {
+                int randomSpot1 = r.Next(AllCards.Count - 1);
+                int randomSpot2 = r.Next(AllCards.Count - 1);
+                //swap to cards at those spots
+                Card temp = AllCards[randomSpot2];
+                AllCards[randomSpot2] = AllCards[randomSpot1];
+                AllCards[randomSpot1] = temp;
+            }
+        }
+    }
+}
---
+++ cardGame/DeckOfCards/Program.cs
@@ -0,0 +1,35 @@
+﻿using DeckOfCards.Classes;
+using System;
+using System.Net.Http.Headers;
+using System.Reflection;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card firstCard = new Card(8, "Hearts");
+
+            Console.WriteLine($"The first card is the {firstCard.faceValue} of {firstCard.suit}.");
+
+            Card secondCard = new Card(4, "Diamonds", true);
+
+            Console.WriteLine($"The second card is the {secondCard.faceValue} of {secondCard.suit}.");
+
+            //Flipping the card from the method in our class
+            secondCard.Flip();
+
+
+
+            //Working with a deck
+            Deck deck = new Deck();
+            for (int i = 0; i < deck.numberOfCardsInDeck; i++)
+            {
+                Card dealCard = deck.Deal1();
+                dealCard.Flip();
+                Console.WriteLine($"{dealCard.faceValue} of {dealCard.suit}");
+            }
+        }
+    }
+}
