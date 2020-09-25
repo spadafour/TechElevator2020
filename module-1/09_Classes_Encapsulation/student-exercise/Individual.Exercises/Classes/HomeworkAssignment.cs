d811fd14b7d8114cd98ea14575bb5eb7361cb634
Rob Spadafore <spadafour@gmail.com>
2020-09-25 11:51:35 -0400

    hw refinements

--- module-1/09_Classes_Encapsulation/student-exercise/Individual.Exercises/Classes/HomeworkAssignment.cs
+++ module-1/09_Classes_Encapsulation/student-exercise/Individual.Exercises/Classes/HomeworkAssignment.cs
@@ -7,7 +7,7 @@ namespace Individual.Exercises.Classes
     public class HomeworkAssignment
     {
         public int EarnedMarks { get; set; }
-        public int PossibleMarks { get; set; }
+        public int PossibleMarks { get; private set; }
         public string SubmitterName { get; private set; }
         public string LetterGrade {
             get 
