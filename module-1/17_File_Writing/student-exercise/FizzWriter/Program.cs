22bf22c84ffa75b46b4ca146aca80d596fd72491
Rob Spadafore <spadafour@gmail.com>
2020-10-06 12:19:09 -0400

    Pushing completed FizzBuzz Writer

--- module-1/17_File_Writing/student-exercise/FizzWriter/Program.cs
+++ module-1/17_File_Writing/student-exercise/FizzWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FizzWriter
 {
@@ -6,7 +7,31 @@ namespace FizzWriter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string directory = Environment.CurrentDirectory;
+            string fileName = "FizzBuzz.txt";
+            string fullPath = Path.Combine(directory, fileName);
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fullPath))
+                {
+                    for (int i = 1; i <= 300; i++)
+                    {
+                        string fizzbuzzResult = "";
+                        if (i % 3 == 0) { fizzbuzzResult += "Fizz"; }
+                        if (i % 5 == 0) { fizzbuzzResult += "Buzz"; }
+                        if (fizzbuzzResult == "") { fizzbuzzResult = $"{i}"; }
+                        sw.WriteLine(fizzbuzzResult);
+                    }
+                }
+                Console.WriteLine($"{fileName} has been created.");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to write to file:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
