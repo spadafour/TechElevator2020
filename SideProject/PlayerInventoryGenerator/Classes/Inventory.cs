86abdaf0d362f5ad5c96817da9958102c34ede1a
Rob Spadafore <spadafour@gmail.com>
2020-10-28 09:24:22 -0400

    Cleaning up

---
+++ SideProject/PlayerInventoryGenerator/Classes/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayerInventoryGenerator.Classes
+{
+    public class Inventory
+    {
+        public int MaxInventory { get; private set; }
+
+        public Inventory(int maxInventory)
+        {
+            MaxInventory = maxInventory;
+        }
+    }
+}
