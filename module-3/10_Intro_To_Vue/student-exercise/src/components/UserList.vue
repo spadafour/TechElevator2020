c542dd889fae35895f43a1ee7476cf77ca3b2143
Rob Spadafore <spadafour@gmail.com>
2020-11-23 15:12:59 -0500

    Pushing hw

--- module-3/10_Intro_To_Vue/student-exercise/src/components/UserList.vue
+++ module-3/10_Intro_To_Vue/student-exercise/src/components/UserList.vue
@@ -11,19 +11,25 @@
     </thead>
     <tbody>
       <tr>
-        <td><input type="text" id="firstNameFilter"/></td>
-        <td><input type="text" id="lastNameFilter"/></td>
-        <td><input type="text" id="usernameFilter"/></td>
-        <td><input type="text" id="emailFilter"/></td>
+        <td><input type="text" id="firstNameFilter" v-model="filter.firstName"/></td>
+        <td><input type="text" id="lastNameFilter" v-model="filter.lastName"/></td>
+        <td><input type="text" id="usernameFilter" v-model="filter.username"/></td>
+        <td><input type="text" id="emailFilter" v-model="filter.emailAddress"/></td>
         <td>
-          <select id="statusFilter">
+          <select id="statusFilter" v-model="filter.status">
             <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Disabled">Disabled</option>
           </select>
         </td>
       </tr>
-      <!-- user listing goes here -->
+      <tr v-for="user in filteredList" v-bind:key="user.id" v-bind:class="{disabled : user.status === 'Disabled'}">
+        <td>{{user.firstName}}</td>
+        <td>{{user.lastName}}</td>
+        <td>{{user.username}}</td>
+        <td>{{user.emailAddress}}</td>
+        <td>{{user.status}}</td>
+      </tr>
     </tbody>
   </table>
 </template>
@@ -40,7 +46,27 @@ export default {
         { firstName: 'Ben', lastName: 'Carter', username: 'bcarter', emailAddress: 'bcarter@gmail.com', status: 'Active' },
         { firstName: 'Katie', lastName: 'Jackson', username: 'kjackson', emailAddress: 'kjackson@yahoo.com', status: 'Active' },
         { firstName: 'Mark', lastName: 'Smith', username: 'msmith', emailAddress: 'msmith@foo.com', status: 'Disabled' }
-      ]
+      ],
+      filter: {
+        firstName: '',
+        lastName: '',
+        username: '',
+        emailAddress: '',
+        status: ''
+      }
+    }
+  },
+  computed: {
+    filteredList() {
+      return this.users.filter (user => {
+        if (user.firstName.toLowerCase().includes(this.filter.firstName.toLowerCase()) && 
+        user.lastName.toLowerCase().includes(this.filter.lastName.toLowerCase()) &&
+        user.username.toLowerCase().includes(this.filter.username.toLowerCase()) &&
+        user.emailAddress.toLowerCase().includes(this.filter.emailAddress.toLowerCase()) &&
+        user.status.includes(this.filter.status)) {
+          return user;
+        }
+      })
     }
   }
 }
